8e8a9bba8a1c9600b963816436efdcc2493577f6
Julien LAVRARD-MEYER <119691230+Julyse@users.noreply.github.com>
2025-06-15 13:20:23 +0100

    Revert "Fix conflicts temp"

--- Assets/Scripts/MovingPlatforme.cs
+++ Assets/Scripts/MovingPlatforme.cs
@@ -1,30 +1,31 @@
 using UnityEngine;
 using System.Collections;
 
-
-
-    
 public class MovingPlatforme : MonoBehaviour
 {
-
-    [SerializeField] GameObject pointA;
-    [SerializeField] GameObject pointB;
-    [SerializeField] float speed = 10f;
-    [SerializeField] float delay = 1f;
-    [SerializeField] GameObject platform;
+    [SerializeField] private GameObject pointA;
+    [SerializeField] private GameObject pointB;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float delay = 1f;
+    [SerializeField] private GameObject platform;
+    [SerializeField] private float startDelay = 0f; // <-- Nouveau champ pour décalage
 
     private Vector3 targetPosition;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         platform.transform.position = pointA.transform.position;
         targetPosition = pointB.transform.position;
-        StartCoroutine(MovePlatform());
-        
+        StartCoroutine(StartWithDelay());
     }
 
-    
-    IEnumerator MovePlatform()
+    private IEnumerator StartWithDelay()
+    {
+        yield return new WaitForSeconds(startDelay);
+        StartCoroutine(MovePlatform());
+    }
+
+    private IEnumerator MovePlatform()
     {
         while (true)
         {
@@ -35,8 +36,9 @@ public class MovingPlatforme : MonoBehaviour
             }
 
             targetPosition = (targetPosition == pointA.transform.position)
-                    ? pointB.transform.position : pointA.transform.position;
+                ? pointB.transform.position : pointA.transform.position;
+
             yield return new WaitForSeconds(delay);
-        }    
+        }
     }
 }
