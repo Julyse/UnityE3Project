50d0b4e097a9a195ef2e19a803d2825ed90fe651
Julien LAVRARD-MEYER <119691230+Julyse@users.noreply.github.com>
2025-06-15 19:08:38 +0100

    dialogue version 1

--- Assets/ConversationStarter.cs
+++ Assets/ConversationStarter.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
-
+using System.Collections;
+using System.Collections.Generic;
+using DialogueEditor;
 public class ConversationStarter : MonoBehaviour
 {
+    [SerializeField] private NPCConversation myConversation; // Référence au scriptable object Dialogue
+    [SerializeField] private bool unlockMouseDuringConversation = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerStay(Collider other)
     {
-        
-    }
+        // Vérifie si le joueur est entré dans le trigger
+        if (other.CompareTag("Player"))
+        {
+            // Vérifie si la touche "E" est pressée
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                // Démarre le dialogue
+                if (unlockMouseDuringConversation)
+{
+    Cursor.lockState = CursorLockMode.None;
+    Cursor.visible = true;
+}
+                ConversationManager.Instance.StartConversation(myConversation);
+            }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
